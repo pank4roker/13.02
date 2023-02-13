b86a4aefae70e5b6873b94850d4bc806c392862c
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2023-02-13 11:17:32 +0200

    add

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //z 1
+            //Console.WriteLine("Размер массива= ");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write($"array[{i}]= ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //int p = array[0];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] += p;
+            //}
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            // z2
+            //int[] array = { 7, 1, 8, -4, -3, 2, 6 };
+            //int count = 0;
+            //foreach (int elem in array)
+            //{
+            //    if (elem % 2 == 0) count++;
+            //}
+            //Console.WriteLine($"count ={count}");
+            // z3
+            //Console.WriteLine("n= ");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //Random rnd = new Random();
+            //for(int i=0;i<array.Length;i++)
+            //{
+            //    array[i] = rnd.Next(-10, 10);
+            //    Console.WriteLine($"array[{i}]={array[i]}");
+            //}
+            //int max = array[0];
+            //for(int i=1;i<array.Length;i++)
+            //{
+            //    if (array[i]>max)
+            //    { max = array[i]; }
+            //}
+            //Console.WriteLine($"max={max}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
